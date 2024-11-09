8382c46f8a01599534593548a8c5fc73d84f97d7
SAIRANTRI <pathaksairantri@gmail.com>
2024-11-09 09:40:07 +0530

    signup and home pages added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
-import Login from "./pages/login/Login"
+import Login from "./pages/login/Login.jsx"
+import Signup from "./pages/signup/Signup.jsx"
+import Home from "./pages/home/Home.jsx"
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Login/>
+      <Home/>
     </div>
   )
 }
