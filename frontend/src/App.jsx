7e726dbd18e73a0b04b914972d6519f501342743
SAIRANTRI <pathaksairantri@gmail.com>
2024-11-10 21:48:22 +0530

    logout endpoint added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import Login from "./pages/login/Login.jsx"
 import Signup from "./pages/signup/Signup.jsx"
 import Home from "./pages/home/Home.jsx"
+import { Navigate, Route, Routes } from "react-router-dom"
+import { useAuthContext } from "./context/useAuthContext.jsx";
 
 function App() {
+  const { authUser } = useAuthContext();
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Home/>
+      <Routes>
+      <Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
+				<Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
+				<Route path='/signup' element={authUser ? <Navigate to='/' /> : <Signup />} />
+        
+      </Routes>
     </div>
   )
 }
